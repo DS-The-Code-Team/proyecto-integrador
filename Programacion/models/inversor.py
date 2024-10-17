9b267cedc92592d1933eb1c0bbab125de3c9058a
matias-saig <saig.matias@gmail.com>
2024-10-17 20:59:58 -0300

    model: inversor, transaccion

--- Programacion/models/inversor.py
+++ Programacion/models/inversor.py
@@ -1,10 +1,12 @@
+
 #Modelamos entidad Inversor - Usuario
 class Inversor:
-    def __init__(self, nombre: str, apellido: str, cuil: int, correo: str, contraseña: str, pin: int, saldo=1000000.00):
+    def __init__(self, id: int, nombre: str, apellido: str, cuil: int, correo: str, contrasena: str, pin: int, saldo=1000000.00):
+        self.id = id
         self.nombre = nombre
         self.apellido = apellido
         self.cuil = cuil
         self.correo = correo
-        self.contraseña = contraseña
+        self.contrasena = contrasena
         self.pin = pin
         self.saldo = saldo
