3ec1ad6bbf36932acbb8115e0d99e6f28852f5d8
matias-saig <saig.matias@gmail.com>
2024-10-18 19:46:49 -0300

    gitignore update

--- Programacion/models/inversor.py
+++ Programacion/models/inversor.py
@@ -1,7 +1,7 @@
 
 #Modelamos entidad Inversor - Usuario
 class Inversor:
-    def __init__(self, id: int, nombre: str, apellido: str, cuil: int, correo: str, contrasena: str, pin: int, saldo= 1000000.00):
+    def __init__(self, id: int, nombre: str, apellido: str, cuil: int, correo: str, contrasena: str, pin: int, saldo:float):
         self.id = id
         self.nombre = nombre
         self.apellido = apellido
