908e140aa692456a24ea8256858513c2a046b140
Francisco <torofrancisco13@gmail.com>
2024-10-27 07:21:40 -0300

    Corrigiendo un print..

--- Programacion/view/listar_inversores_view.py
+++
@@ -1,7 +0,0 @@
-from dao.inversor_dao import InversorDAO
-
-def listar_inversores_view():
-    dao = InversorDAO()
-    inversores = dao.get_all()
-    for inversor in inversores:
-        print(inversor)
